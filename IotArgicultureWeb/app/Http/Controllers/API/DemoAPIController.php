2ec62d8fd7bf6341a1a1d2abb09c5efdd360716c
hopdt1404 <hopdt1404@gmail.com>
2021-03-10 15:34:49 +0700

    #1 Create Base project

---
+++ IotArgicultureWeb/app/Http/Controllers/API/DemoAPIController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\AppBaseController;
+
+/**
+ * Class DemoAPIController
+ * @package App\Http\Controllers\API
+ */
+
+
+class DemoAPIController extends AppBaseController
+{
+
+    public function test() {
+        return view('welcome');
+    }
+}
