b2444835f3cab4e74d3d640ff88c9e20d0319487
hopdt1404 <hopdt1404@gmail.com>
2021-03-16 13:48:06 +0700

    Update demo js

--- IoTAgri/resources/js/router.js
+++ IoTAgri/resources/js/router.js
@@ -2,11 +2,12 @@ import Vue from 'vue';
 import Router from 'vue-router';
 Vue.use(Router);
 
+import firstPage from './src/components/pages/NewPageVue';
 const routes = [
-
     {
         path: '/my-new-vue-route',
-        component: () => import('./src/components/pages/NewPageVue'),
+        // component: () => import('./src/components/pages/NewPageVue'),
+        component: firstPage,
     },
 ];
 export default new Router({
