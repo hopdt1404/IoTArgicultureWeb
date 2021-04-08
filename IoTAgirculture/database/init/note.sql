407ecdefa5b2eb90baade36af5acc1e10863573c
hopdt1404 <hopdt1404@gmail.com>
2021-04-09 06:46:29 +0700

    update DB design

---
+++ IoTAgirculture/database/init/note.sql
@@ -0,0 +1,26 @@
+TABLE:
+
+Controlling: ADD COLUMN create_at, update_at, create_user, update_user : ok, Done
+Devices ___ ADD COLUMN create_at, update_at, create_user, update_user : ok, Done
+DeviceTypes ___ ADD COLUMN create_at, update_at, create_user, update_user : ok Done,
+FarmTypes ___ ADD COLUMN create_at, update_at, create_user, update_user : ok Done,
+Locates ok : Done,
+PlotTypes  ___ ADD COLUMN create_at, update_at, create_user, update_user : ok Done,
+Farms ___ ADD COLUMN  create_at, update_at, create_user, update_user : ok Done,
+Plots ___ ADD COLUMN create_at, update_at, create_user, update_user : ok Done,
+Sensing ___ ADD COLUMN , create_at, create_user: Ok Done,
+WeatherForecastAtATimes ___ ADD COLUMN , create_at,create_user: ok Done
+add table WeatherForecasts ADD COLUMN  create_at, create_user:  ok Done
+Add table User_type TABLE: ok Done
+
+
+Users address, phone, avatar, USER_type_id,  create_at, update_at, create_user, update_user : ok,
+
+
+
+
+
+
+Add TABLE operation_history
+DB LOG (ACTION)
+GET DATA weather
