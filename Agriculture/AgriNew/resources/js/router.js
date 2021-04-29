6cc4be778647d289924f1b3dfa11136afdbc2df9
hopdt1404 <hopdt1404@gmail.com>
2021-04-29 16:14:55 +0700

    #1 Recreate project: update router link

--- Agriculture/AgriNew/resources/js/router.js
+++ Agriculture/AgriNew/resources/js/router.js
@@ -2,8 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 Vue.use(Router)
 
-// import Home from './src/views/home/index'
-import Home from './src/components/home/index'
+import Home from './src/views/home/index'
 import User from './src/views/user/index'
 
 const routes = [
