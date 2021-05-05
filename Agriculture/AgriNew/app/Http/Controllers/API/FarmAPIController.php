a3ab3486bd84f8885c2daece6f42fa79e8450e9b
hopdt1404 <hopdt1404@gmail.com>
2021-05-06 05:32:59 +0700

    #1 Recreate project: create base api

--- Agriculture/AgriNew/app/Http/Controllers/API/FarmAPIController.php
+++ Agriculture/AgriNew/app/Http/Controllers/API/FarmAPIController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\AppBaseController;
 use App\Models\Farm;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+use PHPUnit\Util\Exception;
 
 class FarmAPIController extends AppBaseController
 {
@@ -13,6 +17,26 @@ class FarmAPIController extends AppBaseController
     public function __construct(Farm $farm)
     {
         $this->model = $farm;
+    }
+
+    /**
+     * Store a newly created FavoriteService in storage.
+     * POST /favorite
+     *
+     * @param Request $request
+     *
+     * @throws Exception
+     *
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            return $this->sendSuccess('Success');
+        } catch (Exception $ex) {
+            Log::error('FarmAPIController@store:' . $ex->getMessage().$ex->getTraceAsString());
+            return $this->sendError(Response::$statusTexts[Response::HTTP_INTERNAL_SERVER_ERROR], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
