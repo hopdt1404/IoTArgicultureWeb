598716c021edc1e88249ef59d1ff75e4d2c5e080
hopdt1404 <hopdt1404@gmail.com>
2021-05-15 06:00:28 +0700

    #4 Solve Issue scss and css in project: update webpack.mix.js 01

--- Agriculture/AgriNew/webpack.mix.js
+++ Agriculture/AgriNew/webpack.mix.js
@@ -15,5 +15,21 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
 
 module.exports = {
-
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader', {
+                loader: 'style-resources-loader',
+                options: {
+                    patterns: [
+                        './resource/sass/base_sass/variables/*.scss',
+                        './resource/sass/base_sass/themes/*.scss',
+                        './resource/sass/base_sass/mixins/*.scss',
+                        './resource/sass/layouts/base_layouts/*.scss',
+                        './resource/sass/pages/*.scss',
+                    ]
+                }
+            }]
+        }]
+    },
 }
