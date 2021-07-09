015b3d0f4057d3be36f8fd380d13837ea52b7473
hopdt1404 <hopdt1404@gmail.com>
2021-07-10 06:58:06 +0700

    #7 Create Authenticate in Project: update home.blade.php

--- Agriculture/AgriNew/resources/views/home.blade.php
+++ Agriculture/AgriNew/resources/views/home.blade.php
@@ -2,9 +2,25 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 
+                    {{ __('You are logged in!') }}
+                </div>
+                <div>
+                    Content home.blade
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
