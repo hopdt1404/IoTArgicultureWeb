0a781b46d4923c500c65218b6414fde6736dfb34
hopdt1404 <hopdt1404@gmail.com>
2021-07-11 07:47:33 +0700

    #7 Create Authenticate in Project: update App.vue

--- Agriculture/AgriNew/resources/js/App.vue
+++ Agriculture/AgriNew/resources/js/App.vue
@@ -1,63 +1,70 @@
 <template>
     <div id="app">
-        This is file App.vue
-        // Define navbar component in this file
-        <div>
-            <!--========== ADMIN SIDE MENU one ========-->
-            <div class="_1side_menu" >
-                <div class="_1side_menu_logo">
-                    <h3 style="text-align:center;">Logo Image</h3>
-                    <!--<img src="/img/logo.jpg" style="width: 108px;margin-left: 68px;"/>-->
-                </div>
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
 
-                <!--~~~~~~~~ MENU CONTENT ~~~~~~~~-->
-                <div class="_1side_menu_content">
-                    <div class="_1side_menu_img_name">
-                        <!-- <img class="_1side_menu_img" src="/pic.png" alt="" title=""> -->
-                        <p class="_1side_menu_name">Admin</p>
-                    </div>
-
-                    <!--~~~ MENU LIST ~~~~~~-->
-                    <div class="_1side_menu_list">
-                        <ul class="_1side_menu_list_ul">
-<!--                            <li v-for="(menuItem, i) in permission" :key="i" v-if="permission.length && menuItem.read">-->
-<!--                                <router-link :to="'/'+menuItem.name"><Icon type="ios-speedometer" /> {{menuItem.resourceName}}</router-link>-->
-<!--                            </li>-->
-
-
-                            <li><router-link to="home"><Icon type="md-home" /> Home</router-link></li>
-                            <li><router-link to="user"><Icon type="md-contact" /> User</router-link></li>
-                            <li><router-link to="farm"><Icon type="ios-map" /> Farm</router-link></li>
-                            <li><router-link to="device"><Icon type="md-hammer" /> Device</router-link></li>
-                            <li><router-link to="plant"><Icon type="ios-snow" /> Plant</router-link></li>
-                            <li><router-link to="setting-agriculture"><Icon type="ios-settings" /> Setting Agriculture</router-link></li>
-
-                            <li><a href="/logout"><Icon type="md-arrow-back" /> Logout</a></li>
-
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <!--========== ADMIN SIDE MENU ========-->
-
-            <!--========= HEADER ==========-->
-            <div class="header">
-                <div class="_2menu _box_shadow">
-                    <div class="_2menu_logo">
-                        <ul class="open_button">
-                            <li>
-                                <Icon type="ios-list" />
-                            </li>
-                            <!--<li><Icon type="ios-albums" /></li>-->
-                        </ul>
-                    </div>
+                <a class="navbar-brand" href="/">
+                        Iot Agri
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                    </ul>
+                    <ul class="navbar-nav ml-auto" v-if="userId == null">
+                        <li class="nav-item">
+                            <a class="nav-link" href="login">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href='register'>Register</a>
+                        </li>
+                    </ul>
+                    <ul v-else>
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ username}}
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="logout"
+                                   onclick="logout(event)">
+                                    Logout
+                                </a>
+                                <form id="logout-form" action="logout" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
-            <!--========= HEADER ==========-->
-        </div>
+
+        </nav>
         <router-view></router-view>
     </div>
 </template>
+
+<script>
+// import
+export default {
+    data() {
+        return {
+            userId: null,
+            username: ''
+        }
+    },
+    created() {
+
+    },
+    methods: {
+        logout (event) {
+            event.preventDefault();
+            document.getElementById('logout-form').submit();
+        }
+    }
+
+}
+</script>
 
 
 <style lang="scss">
